41537a4048eb63ba30eea36a2b0977ecde3393a3
samuelwsz <samuelxwsz@gmail.com>
2023-11-22 22:03:52 -0300

    att

--- src/pages/transactions.tsx
+++ src/pages/transactions.tsx
@@ -1,8 +1,31 @@
+import { useEffect, useState } from "react"
 import Header from "../components/header"
 import Summary from "../components/summary"
 import SearchForm from "./components/searchForm"
 
+interface Transaction {
+  id: number
+  description: string
+  type: "income" | "outcome"
+  price: number
+  category: string
+  createdAt: string
+}
+
 export default function Transactions() {
+  const [transactions, setTransactions] = useState<Transaction[]>([])
+
+  async function loadTransactions() {
+    const response = await fetch("http://localhost:3000/transactions")
+    const data = await response.json()
+
+    setTransactions(data)
+  }
+
+  useEffect(() => {
+    loadTransactions()
+  }, [])
+
   return (
     <>
       <div>
@@ -11,30 +34,34 @@ export default function Transactions() {
         <SearchForm />
         <table className="w-4/5 m-auto">
           <tbody className="text-white">
-            <tr className="bg-gray-700 flex flex-col md:flex-row lg:flex-row md:gap-16 lg:gap-28 xl:gap-56 p-3 my-3 rounded-md">
-              <td className="w-full md:w-2/4 lg:w-2/4 mb-2 lg:mb-0">
-                Desenvolvimento de site
-              </td>
-              <td className="flex-shrink-0">R$12.000,00</td>
-              <td className="flex-shrink-0">Venda</td>
-              <td className="flex-shrink-0">13/04/2022</td>
-            </tr>
-            <tr className="bg-gray-700 flex flex-col md:flex-row lg:flex-row md:gap-16 lg:gap-28 xl:gap-56 p-3 my-3 rounded-md">
-              <td className="w-full md:w-2/4 lg:w-2/4 mb-2 lg:mb-0">
-                Desenvolvimento de site
-              </td>
-              <td className="flex-shrink-0">- R$12.000,00</td>
-              <td className="flex-shrink-0">Venda</td>
-              <td className="flex-shrink-0">13/04/2022</td>
-            </tr>
-            <tr className="bg-gray-700 flex flex-col md:flex-row lg:flex-row md:gap-16 lg:gap-28 xl:gap-56 p-3 my-3 rounded-md">
-              <td className="w-full md:w-2/4 lg:w-2/4 mb-2 lg:mb-0">
-                Desenvolvimento de site
-              </td>
-              <td className="flex-shrink-0">R$12.000,00</td>
-              <td className="flex-shrink-0">Venda</td>
-              <td className="flex-shrink-0">13/04/2022</td>
-            </tr>
+            {transactions.map((transaction) => {
+              // Converter a string de data para um objeto Date
+              const dateObject = new Date(transaction.createdAt)
+
+              // Obter as partes da data
+              const year = dateObject.getFullYear()
+              const month = (dateObject.getMonth() + 1)
+                .toString()
+                .padStart(2, "0")
+              const day = dateObject.getDate().toString().padStart(2, "0")
+
+              // Formatar a data no formato desejado (no exemplo, YYYY-MM-DD)
+              const formattedDate = `${day}-${month}-${year}`
+
+              return (
+                <tr
+                  className="bg-gray-700 flex flex-col md:flex-row lg:flex-row md:gap-16 lg:gap-28 xl:gap-56 p-3 my-3 rounded-md"
+                  key={transaction.id}
+                >
+                  <td className="w-full md:w-2/4 lg:w-2/4 mb-2 lg:mb-0">
+                    {transaction.description}
+                  </td>
+                  <td className="flex-shrink-0">R${transaction.price}</td>
+                  <td className="flex-shrink-0">{transaction.category}</td>
+                  <td className="flex-shrink-0">{formattedDate}</td>
+                </tr>
+              )
+            })}
           </tbody>
         </table>
       </div>
